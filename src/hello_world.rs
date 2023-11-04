28202f1a24b01511c6bf48c7879c21d597479031
saboya <gabrielsaboya@live.com>
2023-11-04 08:39:14 -0300

    readme

--- src/hello_world.rs
+++ src/hello_world.rs
@@ -3,7 +3,7 @@ use std::{thread, time};
 
 pub fn hello_world() {  
     let hello_world = "Hello World";
-    let ten_millis = time::Duration::from_millis(10);
+    let fifteen_millis = time::Duration::from_millis(15);
     println!("{:?}",hello_world.as_bytes());
     let mut moving: [u8; 11] = [32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32];
     let buffer: [u8; 11] = [72, 69, 76, 76, 79, 32, 87, 79, 82, 76, 68];
@@ -14,7 +14,7 @@ pub fn hello_world() {
     moving[n] += 1;                                 
     let s2 = std::str::from_utf8(&moving[..]).expect("invalid utf-8 sequence");
     println!("{}", s2);
-    thread::sleep(ten_millis);
+    thread::sleep(fifteen_millis);
     }
 }
 }
