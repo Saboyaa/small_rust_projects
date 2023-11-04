4bd085bf08d7254ca5023f2f2bfa54973d64ad55
saboya <gabrielsaboya@live.com>
2023-11-04 08:09:56 -0300

    adding hello world and guessing game

---
+++ target/debug/deps/Training-d46cd896126b4c89.d
@@ -0,0 +1,6 @@
+/home/saboya/Code/Rust/small_rust_projects/target/debug/deps/Training-d46cd896126b4c89.rmeta: src/main.rs src/hello_world.rs
+
+/home/saboya/Code/Rust/small_rust_projects/target/debug/deps/Training-d46cd896126b4c89.d: src/main.rs src/hello_world.rs
+
+src/main.rs:
+src/hello_world.rs:
